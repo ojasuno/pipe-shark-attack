4d85a7778dec52af05342ca202369d637bdc970d
ojasuno <ojasuno21@gmail.com>
2025-02-24 13:31:13 +1100

    app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type React from "react"
 import "@/app/globals.css"
 import { Inter } from "next/font/google"
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] })
 
