340d5dface894dcff68456c97b052bd2df6ca1b5
ojasuno <ojasuno21@gmail.com>
2025-02-24 16:37:17 +1100

    postcss.config.mjs

--- hooks/use-toast.ts
+++ hooks/use-toast.ts
@@ -152,17 +152,39 @@ function toast({ ...props }: Toast) {
     })
   const dismiss = () => dispatch({ type: "DISMISS_TOAST", toastId: id })
 
+  interface AddToastAction {
+    type: ActionType["ADD_TOAST"];
+    toast: ToasterToast;
+  }
+
+  interface UpdateToastAction {
+    type: ActionType["UPDATE_TOAST"];
+    toast: Partial<ToasterToast>;
+  }
+
+  interface DismissToastAction {
+    type: ActionType["DISMISS_TOAST"];
+    toastId?: ToasterToast["id"];
+  }
+
+  interface RemoveToastAction {
+    type: ActionType["REMOVE_TOAST"];
+    toastId?: ToasterToast["id"];
+  }
+
+  type Action = AddToastAction | UpdateToastAction | DismissToastAction | RemoveToastAction;
+
   dispatch({
     type: "ADD_TOAST",
     toast: {
       ...props,
       id,
       open: true,
-      onOpenChange: (open) => {
-        if (!open) dismiss()
+      onOpenChange: (open: boolean) => {
+        if (!open) dismiss();
       },
     },
-  })
+  });
 
   return {
     id: id,
